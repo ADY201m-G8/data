9b8e3225c374b1d07a06940317de1967ca399e73
sainoman15 <chamkhoat@gmail.com>
2026-06-22 16:27:25 +0700

    feat: add table subject to db.sqlite

--- src/preprocess/_5_build_sqlite.py
+++ src/preprocess/_5_build_sqlite.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from src import DB_FOLDER_PATH, PROCESSED_DATA_PATH
 
+SUBJECTS = ["ADY201m", "JPD123", "MAI391", "MAS291"]
 
 def process_csv(path: Path) -> list[tuple]:
     with path.open(mode="r", encoding="utf-8") as f:
@@ -21,34 +22,51 @@ def init_db(cursor: sqlite3.Cursor) -> None:
         CREATE TABLE IF NOT EXISTS student (
             [id] TEXT PRIMARY KEY,
             [fullname] TEXT NOT NULL
-        )
+        );
         """
     )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS subject (
+            [id] TEXT PRIMARY KEY
+        );
+        """
+    )
+            
 
 
-def insert_students(cursor: sqlite3.Cursor, data: list[tuple]) -> None:
+def insert_rows(cursor: sqlite3.Cursor, students: list[tuple], subjects: list[str]) -> None:
     cursor.executemany(
         """
-        INSERT OR REPLACE INTO student (id, fullname)
+        INSERT INTO student (id, fullname)
         VALUES (?, ?)
         """,
-        data,
+        students,
+    )
+    formatted_subjects = [(subject,) for subject in subjects]
+    cursor.executemany(
+        """
+        INSERT INTO subject (id)
+        VALUES (?)
+        """,
+        formatted_subjects,
     )
 
 
-def save_to_sqlite(db_path: Path, data: list[tuple[str, str]]) -> None:
+def save_to_sqlite(db_path: Path, students: list[tuple], subjects: list[str]) -> None:
     os.makedirs(DB_FOLDER_PATH, exist_ok=True)
+    os.remove(db_path) if db_path.exists() else None
 
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         init_db(cursor)
-        insert_students(cursor, data)
+        insert_rows(cursor, students, subjects)
         conn.commit()
 
 
 def main():
     data = process_csv(PROCESSED_DATA_PATH / "students.csv")
-    save_to_sqlite(DB_FOLDER_PATH / "db.sqlite3", data)
+    save_to_sqlite(DB_FOLDER_PATH / "db.sqlite3", data, SUBJECTS)
 
 
 if __name__ == "__main__":
